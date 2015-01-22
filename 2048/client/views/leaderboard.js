9cfcd77f28cd555cecc15a114fe68bf9d8e083e1
Jake Lin <jakelinau@gmail.com>
2015-01-23 09:37:43 +1100

    Create Scores collection

--- 2048/client/views/leaderboard.js
+++ 2048/client/views/leaderboard.js
@@ -1,6 +1,5 @@
 Template.leaderboard.helpers({
-  scores:[{name:'Jake', currentScore: 20, bestScore: 50},
-    {name:'Ken', currentScore: 32, bestScore: 34},
-    {name:'Mark', currentScore: 23, bestScore: 32}
-  ]
+  scores:function(){
+    return Scores.find({});
+  }
 });

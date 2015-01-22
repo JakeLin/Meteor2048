5bc1e6b6f7a335c2b30ea6453488c43863c23318
Jake Lin <jakelinau@gmail.com>
2015-01-23 08:47:25 +1100

    Add sample data to leaderboard template

---
+++ 2048/client/views/leaderboard.js
@@ -0,0 +1,6 @@
+Template.leaderboard.helpers({
+  scores:[{name:'Jake', currentScore: 20, bestScore: 50},
+    {name:'Ken', currentScore: 32, bestScore: 34},
+    {name:'Mark', currentScore: 23, bestScore: 32}
+  ]
+});

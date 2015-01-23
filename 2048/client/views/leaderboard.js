91e3061743a81305de481ab82245279fceb90f35
Jake Lin <jakelinau@gmail.com>
2015-01-23 00:43:07 +0000

    Add account package

--- 2048/client/views/leaderboard.js
+++ 2048/client/views/leaderboard.js
@@ -3,3 +3,9 @@ Template.leaderboard.helpers({
     return Scores.find({});
   }
 });
+
+/*
+Template.leaderboard.helpers({
+  scores:[{name:'Jake', currentScore:23, bestScore:65},{name:'Ken', currentScore:31, bestScore:51},{name:'Mark', currentScore:213, bestScore:615}]
+});
+*/
